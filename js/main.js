7060b402fa3d5d81bcae25737dbc74848bf30186
Juana Rodriguez <rodriguezjuana212@gmail.com>
2025-09-18 17:39:10 -0500

    Agregue las constantes globales para los elementos principales

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const input =document.getElementsByClassName("form-control");
+const label =document.getElementsByClassName("form-label");
+const vehiculoForm =document.getElementById("vehiculo-form");
+const itemVehiculo =document.getElementsByClassName("col-md-6 item-vehiculo");
+
+function CreateVehiculoFrom(textoVehiculo){
+    const vehiculo =document.createElement('div');
+    
+}
